7d02bd3678421778706144c3b5d319fdb143130c
HokutoMorita <han2210mh@gmail.com>
2021-08-07 12:35:13 +0900

    [add]Qiitaユーザーデータ収集ETL処理の追加

--- data_collection/src/transform/transform_qiita.py
+++ data_collection/src/transform/transform_qiita.py
@@ -49,3 +49,37 @@ def rename_columns(df, target__columns):
     データフレームから対象のカラム名を変更する
     """
     return df.rename(columns=target__columns)
+
+def transform_qiita_user():
+    """
+    Qiitaのユーザーデータを整形する
+    """
+    qiita_item_df = pd.read_csv('../data/extracted/extracted_qiita_item.csv')
+
+    qiita_user_df = create_qiita_user_df(qiita_item_df)
+
+    required_columns = [
+        'description', 'facebook_id', 'followees_count', 'followers_count',
+        'github_login_name', 'id', 'items_count', 'linkedin_id', 'location',
+        'name', 'organization', 'permanent_id', 'profile_image_url',
+        'team_only', 'twitter_screen_name', 'website_url'
+    ]
+    qiita_user_df = extract_required_columns(qiita_user_df, required_columns)
+
+    rename_target_columns = {
+        'id': 'user_id'
+    }
+    qiita_user_df = rename_columns(qiita_user_df, rename_target_columns)
+
+    return qiita_user_df
+
+def create_qiita_user_df(qiita_item_df):
+    """
+    qiita_item_dfからqiita_user_dfを生成する
+    """
+    qiita_user_str_list = qiita_item_df['user'].to_list()
+    qiita_user_dict_list = []
+    for qiita_user_str in qiita_user_str_list:
+        qiita_user_dict = ast.literal_eval(qiita_user_str)
+        qiita_user_dict_list.append(qiita_user_dict)
+    return pd.DataFrame.from_records(qiita_user_dict_list)
