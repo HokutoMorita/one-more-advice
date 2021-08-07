fa597132caabebfd3358b8fedcb03768230ee145
HokutoMorita <han2210mh@gmail.com>
2021-08-07 11:44:57 +0900

    [add]Qiita投稿データにユーザーIDを付与

--- data_collection/src/transform/transform_qiita.py
+++ data_collection/src/transform/transform_qiita.py
@@ -1,5 +1,4 @@
-import os
-import requests
+import ast
 import pandas as pd
 
 from work_flow_logging import work_flow_logging
@@ -14,12 +13,14 @@ def transform_qiita_item():
     """
     qiita_item_df = pd.read_csv('../data/extracted/extracted_qiita_item.csv')
 
+    qiita_item_df['user_id'] = qiita_item_df['user'].map(extract_user_id)
+
     required_columns = ['id', 'body', 'coediting', 'comments_count', 
         'likes_count', 'private', 'reactions_count', 
-        'title', 'url', 'page_views_count'
+        'title', 'url', 'page_views_count', 'user_id'
     ]
     qiita_item_df = extract_required_columns(qiita_item_df, required_columns)
-    qiita_item_df['user_id'] = 'hoge'
+
     rename_target_columns = {
         'id': 'item_id',
         'body': 'text'
@@ -27,6 +28,15 @@ def transform_qiita_item():
     qiita_item_df = rename_columns(qiita_item_df, rename_target_columns)
 
     return qiita_item_df
+
+def extract_user_id(user_dict_str):
+    """
+    QiitaユーザーIDを抽出する
+    辞書型の文字列を辞書型に変換する
+    辞書からQiitaユーザーIDを取得する
+    """
+    user_dict = ast.literal_eval(user_dict_str)
+    return user_dict['id']
 
 def extract_required_columns(df, required_columns):
     """
